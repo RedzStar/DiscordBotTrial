f2680a35c1b00ccfd296703e07784abd2d12b52c
RedzStar <67134079+RedzStar@users.noreply.github.com>
2020-07-07 13:34:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { prefix, token } = require("./config.json")
 
-const client = new Discord.Client();
+const bot = require("./handler/ClientBuilder.js"); // We're gonna create this soon.
+const client = new bot();
+
+const { VultrexDB } = require("vultrex.db")
+
+const db = new VultrexDB({
+    provider: "sqlite",
+    table: "main",
+    fileName: "main"
+});
+
+db.connect().then(() => {
+    client.prefix = new Object();
+    client.prefix["default"] = prefix;
+    client.db = db;
+
+    require("./handler/module.js")(client);
+    require("./handler/event.js")(client);
+
+    client.package = require("./package.json");
+    client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
+    client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
 
 
-require("./handler/module.js")(client);
-require("./handler/Event.js")(client);
+    client.login(token);
 
-
-client.package = require("./package.json");
-client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
-client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
-
-
-
-
-client.once('ready', () => {
-    console.log('The bot is online!');
 });
 
 
-client.login(token);
